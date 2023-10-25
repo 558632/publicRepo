8ff7ce806cf5215238fd84371a5abf0643f6159a
558632 <91661473+558632@users.noreply.github.com>
2023-10-25 17:28:45 +0200

    Update customScriptExtension_01.ps1

--- customScriptExtension_01.ps1
+++ customScriptExtension_01.ps1
@@ -6,3 +6,4 @@ $nic = Get-aznetworkinterface -name $AZVMNic
 $nic.Dnssettings.dnsservers.add("172.21.0.22")
 $nic | Set-AzNetworkInterface
 restart-azvm -resourcegroupname Lukas_RG -name $AZVM
+stop-azvm -resourcegroupname Lukas_RG -name $AZVM
